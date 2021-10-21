310f49a42ea6568eff1e5961d418e2205e55a102
TiagoALopes <tiagoandresllopes@hotmail.com>
2021-10-21 02:49:17 -0300

    Disponibilização da tarefa 02

--- 01_Infra_e_Cloud/terraform/variables.tf
+++ 01_Infra_e_Cloud/terraform/variables.tf
@@ -1,3 +1,8 @@
+#Autor: Tiago André Lopes
+#Email: tiago.lopes@aluno.faculdadeimpacta.com.br
+#Disciplina: Infrastructure and Cloud Computing
+#Aula 02: Conexão com o SSH da Máquina Virtual.
+
 variable "user" {
   description = "SSH user"
 }
